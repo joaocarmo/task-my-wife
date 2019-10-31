ca30a64cbad72eefb021488b7dc498b99e976fdb
Joao Carmo <kingjoao@gmail.com>
2019-10-31 13:24:40 +0000

    Added more tests

--- routes/__tests__/response-message.js
+++ routes/__tests__/response-message.js
@@ -7,6 +7,47 @@ describe('Examine the function that formats the server response', () => {
       message: '',
       data: [],
     })
+    expect(responseMsg({})).toEqual({
+      status: 'success',
+      message: '',
+      data: [],
+    })
+  })
+  it('should return an empty success message with invalid arguments', () => {
+    expect(responseMsg('invalid')).toEqual({
+      status: 'success',
+      message: '',
+      data: [],
+    })
+    expect(responseMsg({ key: 'isInvalidToo' })).toEqual({
+      status: 'success',
+      message: '',
+      data: [],
+    })
+    expect(responseMsg({ status: 'isInvalidToo', message: 0 })).toEqual({
+      status: 'success',
+      message: '',
+      data: [],
+    })
+  })
+  it('should return an error message with the error/message arguments', () => {
+    expect(responseMsg({ status: 'error' })).toEqual({
+      status: 'error',
+      message: '',
+      data: [],
+    })
+    expect(responseMsg({ status: 'error', message: 'details' })).toEqual({
+      status: 'error',
+      message: 'details',
+      data: [],
+    })
+  })
+  it('should return a success message with data only', () => {
+    expect(responseMsg({ data: [1, 2, 3] })).toEqual({
+      status: 'success',
+      message: '',
+      data: [1, 2, 3],
+    })
   })
 })
 
